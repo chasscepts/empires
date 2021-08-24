b511c553b26339f4cf44c4348fb263b732a74a92
Obetta <francischassobetta@gmail.com>
2021-08-24 07:33:32 +0100

    Style error page

--- src/features/empires/components/ErrorPage.js
+++ src/features/empires/components/ErrorPage.js
@@ -1,17 +1,46 @@
 import PropTypes from 'prop-types';
 
 const styles = {
-  h1: {
-    textAlign: 'center',
-    marginTop: '40px',
+  wrap: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    color: '#fff',
+  },
+  headingWrap: {
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: '2px solid #c31212',
+  },
+  heading: {
+    fontWeight: 'bold',
+    fontSize: '1.6rem',
+    marginLeft: '10px',
+    color: '#c31212',
+  },
+  svg: {
+    width: '24px',
+    height: '24px',
+  },
+  message: {
+    fontSize: '1.4rem',
+    margin: '15px 0 0',
   },
 };
 
 export default function ErrorPage({ message }) {
   return (
-    <>
-      <h1 style={styles.h1}>{message}</h1>
-    </>
+    <div className="body-fill" style={styles.wrap}>
+      <div>
+        <div style={styles.headingWrap}>
+          <svg style={styles.svg} viewBox="0 0 24 24">
+            <path fill="#c31212" d="M13 14H11V9H13M13 18H11V16H13M1 21H23L12 2L1 21Z" />
+          </svg>
+          <div style={styles.heading}>ERROR</div>
+        </div>
+        <p style={styles.message}>{message}</p>
+      </div>
+    </div>
   );
 }
 
