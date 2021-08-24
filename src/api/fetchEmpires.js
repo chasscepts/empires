468d7a7687073a5ecbb4cf07496f5259b99b5d0a
Obetta <francischassobetta@gmail.com>
2021-08-24 07:37:10 +0100

    Connect to API

---
+++ src/api/fetchEmpires.js
@@ -0,0 +1,48 @@
+// import empires from './empires';
+
+// export default function fetchEmpires() {
+//   return new Promise((resolve) => {
+//     setTimeout(() => {
+//       resolve(empires.civilizations);
+//     }, 5000);
+//   });
+// }
+
+const url = 'https://arcane-temple-01740.herokuapp.com/https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations';
+
+// export default function fetchEmpires() {
+//   return  new Promise((resolve, reject) => {
+//     fetch(url, { mode: 'no-cors' })
+//       .then((res) => {
+//         if (!res.ok) throw new Error(`Api responded with ${res.status}`);
+//         return res.json();
+//       })
+//       .then((json) => {
+//         resolve(json.civilizations);
+//       })
+//       .catch((err) => {
+//         reject(err);
+//       });
+//   });
+// }
+
+export default function fetchEmpires() {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('get', url);
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        try {
+          const response = JSON.parse(xhr.response);
+          resolve(response.civilizations);
+        } catch (err) {
+          reject(err);
+        }
+      } else {
+        reject(xhr.response);
+      }
+    };
+
+    xhr.send();
+  });
+}
